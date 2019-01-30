3d8f271a5b9ac8636644405e4b8158580bd0b95d
wuhx <546446588@qq.com>
2019-01-30 16:38:52 +0800

    one

--- lib/main.dart
+++ lib/main.dart
@@ -6,82 +6,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ///item_view
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              Text(
-                'Kandersteg,Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              )
-            ],
-          )),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41')
-        ],
-      ),
-    );
-
-    ///item_view
-    Column buildButtonColumn(IconData icon, String label) {
-      Color color = Theme.of(context).primaryColor;
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(
-            icon,
-            color: color,
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              label,
-              style: TextStyle(
-                  fontSize: 12.0, fontWeight: FontWeight.w400, color: color),
-            ),
-          )
-        ],
-      );
-    }
-
-    //三个并排按钮
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          buildButtonColumn(Icons.call, 'Call'),
-          buildButtonColumn(Icons.near_me, 'Route'),
-          buildButtonColumn(Icons.share, 'Share'),
-        ],
-      ),
-    );
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32.0),
-      child: Text(
-        '''
-        Lake Oeschinen lises at the foot of the Blüemlisalp in the Bernese Alps, Situated 1,578 meters above sea level,it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest,leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
-        ''',
-        softWrap: true,
-      ),
-    );
     return new MaterialApp(
       title: 'Flutter Four Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
@@ -97,9 +21,6 @@ class MyApp extends StatelessWidget {
               height: 240.0,
               fit: BoxFit.cover,
             ),
-            titleSection,
-            buttonSection,
-            textSection,
           ],
         ),
       ),
